6fd2e1a8403aefbfc92b02dac5884913a620f57b
Ryan Farrow <rfarrow1010@gmail.com>
2021-04-13 20:55:08 -0400

    initial file

---
+++ ds_on_batch.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+
+def usage():
+    print('''Usage:
+    ./ds_on_batch.py file.mp4 -m numberOfMachines 
+    ''')
+    exit(0)
+
+def splitAudio(num):
+    pass
+
+if __name__ == "__main__":
+    # run script here
+    # start by parsing command line
+    pass
