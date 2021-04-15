8426ca4b721ac776a3d9ef6e81e44ac367f7afe3
Ryan Farrow <rfarrow1010@gmail.com>
2021-04-14 22:08:17 -0400

    added comments for plan + some code

--- ds_on_batch.py
+++ ds_on_batch.py
@@ -28,15 +28,20 @@ def check_dependencies():
         usage()
 
 
-def split_audio(filename, num):
+def split_audio(filename):
     '''
-    Splits up the given audio file into the given number of equally sized
+    Splits up the given audio file into
     smaller audio files using ffmpeg. The files will appear in the working
     directory and will be named procedurally. Returns void.
     '''
     # first, need to get the total length of the input file in seconds. can be done like this:
     # mp3info -p "%S\n" filename
-    pass
+    # NOTE: if we just split everything up into 1 or 2-minute segments, we might not need mp3info
+    # after all
+
+    # https://unix.stackexchange.com/questions/280767/how-do-i-split-an-audio-file-into-multiple
+
+    subprocess.run(f"ffmpeg -i {filename} -f segment -segment_time 60 -c copy dsfile%03d.mp3")
 
 
 def clean_working_directory():
@@ -46,6 +51,7 @@ def clean_working_directory():
     '''
     # we are going to assume that the filenames can range from dsfile000.mp3 to dsfile999.mp3
     # NOTE: just a first draft, check that this works
+    # this will also have to clean out the input S3 bucket
     for i in range(1000):
         subprocess.run(["rm", f"dsfile{i:03d}.mp3"])
 
@@ -58,7 +64,18 @@ def main():
 
     # parse command line
     audio_filename = sys.argv[1]
-    number_of_machines = int(sys.argv[3])
+    # number_of_machines = int(sys.argv[3])
+
+    # on second thought: thinking the easiest way of doing this is to use AWS Lambda and chop up the 
+    # audio files into tiny, 1 to 2-minute pieces
+    # we can then upload each of those to S3 and trigger the Lambda function, which will run DeepSpeech
+    # on it and send the output to another bucket, from which our client can then fetch the output
+    # example of something similar:
+    # https://docs.aws.amazon.com/lambda/latest/dg/with-s3-example.html
+    # we just need to capture the output in a text file and then put it into the output S3 bucket like so:
+    # https://stackoverflow.com/questions/49163099/writing-a-file-to-s3-using-lambda-in-python-with-aws
+    # our output bucket is on Ryan's account and I'll fetch the details at some point
+
     
 
 if __name__ == "__main__":
